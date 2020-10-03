497d1a92d98318ddedae9d03e74c72cdd1532725
Nestop <fednestor99@gmail.com>
2020-10-03 15:21:36 +0300

    Vector2Dint
    2D representation of int vector.

---
+++ Assets/Scripts/Utils/Vector/Vector2Dint.cs
@@ -0,0 +1,27 @@
+﻿namespace Utils
+{
+    public class Vector2Dint
+    {
+       public int x;
+       public int y;
+
+       public Vector2Dint(int x, int y)
+       {
+           this.x = x;
+           this.y = y;
+       }
+
+        public override bool Equals(object obj)
+        {
+            var vector = obj as Vector2Dint;
+            return x == vector.x && y == vector.y;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashcode = x.GetHashCode();
+            hashcode = 31*hashcode + y.GetHashCode();
+            return hashcode;
+        }
+    }
+}
