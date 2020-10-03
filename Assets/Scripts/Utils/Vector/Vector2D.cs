606dedc0294f4b068c28e18356efe2d698e43939
Nestop <fednestor99@gmail.com>
2020-10-03 15:05:56 +0300

    Vector2D
    2D representation of float vector.

---
+++ Assets/Scripts/Utils/Vector/Vector2D.cs
@@ -0,0 +1,25 @@
+﻿namespace Utils
+{
+    public class Vector2D
+    {
+       public float x;
+       public float y;
+
+       public Vector2D(float x, float y)
+       {
+           this.x = x;
+           this.y = y;
+       }
+
+        public override bool Equals(object obj)
+        {
+            var vector = obj as Vector2D;
+            return x == vector.x && y == vector.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return (x.GetHashCode() + 32*y.GetHashCode());
+        }
+    }
+}
